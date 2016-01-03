405260ff38e31fa9f65ed839d773057768f78778
Matthew Robb <mrobb.dev@gmail.com>
2016-01-03 18:33:53 -0500

    Got things working correctly but now just using PostCSS

--- index.js
+++ index.js
@@ -2,70 +2,62 @@
 
 var through = require('through2');
 var path = require('path');
-var bless = require('bless');
 var gutil = require('gulp-util');
-var merge = require('merge');
+var postcss = require('postcss');
 var File = gutil.File;
 var PluginError = gutil.PluginError;
 
+var chunk = require('./lib/chunk');
+
 module.exports = function(options){
-    var pluginName = 'gulp-bless';
-    var defaults = {
-        cacheBuster: true,
-        cleanup: true,
-        compress: false,
-        force: false,
-        imports: true
-    };
+  var pluginName = 'gulp-bless';
 
-    options = merge(true, defaults, options);
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) return cb(null, file); // ignore
+    if (file.isStream()) return cb(new PluginError(pluginName,  'Streaming not supported'));
 
-    return through.obj(function(file, enc, cb) {
-        if (file.isNull()) return cb(null, file); // ignore
-        if (file.isStream()) return cb(new PluginError(pluginName,  'Streaming not supported'));
+    var stream = this;
+    var contents;
 
-        var stream = this;
+    if (file.contents && (contents = file.contents.toString('utf8'))) {
+      var fileName = path.basename(file.path, '.css');
 
-        if (file.contents && file.contents.toString()) {
-            var fileName = path.basename(file.path);
-            var outputFilePath = path.resolve(path.dirname(file.path), fileName);
-            var contents = file.contents.toString('utf8');
-            var blessOpts = merge(true, options);
-            if(typeof blessOpts.log !== 'undefined') delete blessOpts.log
+      postcss([ chunk({ filename: fileName }) ]).process(contents).then(function(result) {
 
-            new (bless.Parser)({
-                output: outputFilePath,
-                options: blessOpts
-            }).parse(contents, function(err, blessedFiles, numSelectors) {
-                    if (err) {
-                        return cb(new PluginError(pluginName,  err));
-                    }
+        var numSelectors = result.num_selectors;
+        var blessedFiles = result.chunks;
 
-                    if (options.log) {
-                        // print log message
-                        var msg = 'Found ' + numSelectors + ' selector' + (numSelectors === 1 ? '' : 's') + ', ';
-                        if (blessedFiles.length > 1) {
-                            msg += 'splitting into ' + blessedFiles.length + ' blessedFiles.';
-                        } else {
-                            msg += 'not splitting.';
-                        }
-                        gutil.log(msg);
-                    }
-
-                    // write processed file(s)
-                    blessedFiles.forEach(function (blessedFile) {
-                        stream.push(new File({
-                            cwd: file.cwd,
-                            base: file.base,
-                            path: path.resolve(blessedFile.filename),
-                            contents: new Buffer(blessedFile.content)
-                        }));
-                    });
-
-                    cb();
-                });
-        } else {
-            return cb(null, file);
+        if (options.log) {
+          // print log message
+          var msg = 'Found ' + numSelectors + ' selector' + (numSelectors === 1 ? '' : 's') + ', ';
+          if (blessedFiles.length > 1) {
+            msg += 'splitting into ' + blessedFiles.length + ' blessedFiles.';
+          } else {
+            msg += 'not splitting.';
+          }
+          gutil.log(msg);
         }
-    });
+
+        // write processed file(s)
+        blessedFiles.forEach(function (blessedFile, idx) {
+          var newName = fileName + '.part' + (idx+1) + '.css';
+
+          var newFile = new File({
+            cwd: file.cwd,
+            base: file.base,
+            path: path.resolve(path.dirname(file.path), newName),
+            contents: new Buffer(blessedFile.css)
+          });
+
+          stream.push(newFile);
+        });
+
+        cb();
+      }, function(err) {
+        return cb(new PluginError(pluginName,  err));
+      });
+    } else {
+      return cb(null, file);
+    }
+  });
 };
